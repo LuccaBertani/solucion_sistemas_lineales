97091333a0395216a67fcb23a20b38b22d5b9b2c
LuccaBertani <luccajbertani@gmail.com>
2025-06-20 14:27:38 -0300

    El sistema admite los dos metodos para sistemas nxn

--- CalculoSistemasLineales.cpp
+++ CalculoSistemasLineales.cpp
@@ -4,7 +4,8 @@
 
 using namespace std;
 
-float calculadoraCoeficientes(const list<float>& coeficientes, const list<float>& valores_anteriores, int posicionIncognita);
+float calculadoraCoeficientesJacobi(const list<float>& coeficientes, const list<float>& valores_anteriores, int posicionIncognita);
+float calculadoraCoeficientesGauss_Seidel(const list<float>& coeficientes, const list<float>& valores_anteriores, int posicionIncognita, const list<float>& valores_actuales);
 
 int main(){
 
@@ -18,7 +19,7 @@ cin >> rtaSist;
 cout << "Elegir metodo de resolucion:\n 1_Jacobi\n 2_Gauss-Seidel\n";
 cin >> rtaMetodo;
 
-cout << "Teniendo en cuenta el formato ax + bx + cx ... +zx = k" << endl;
+cout << "Teniendo en cuenta el formato ax + by + cz ... +zr = k" << endl;
 
 for(int j = 0; j< rtaSist ; j++){
 
@@ -44,9 +45,9 @@ coeficientes.push_back(valor);
 ecuacionesConCoeficientes.push_back(coeficientes);
 }
 
-if(rtaMetodo == 1){
-
 int iteracion = 0;
+
+if(rtaMetodo == 1){
 
 list <float> incognitasAnteriores(rtaSist,0);
 list<float> incognitasActuales;
@@ -54,7 +55,7 @@ list<float> incognitasActuales;
 while (iteracion != -1){
 int t = 0;
 for (const list<float>& ecuacion : ecuacionesConCoeficientes) {
-        float incognita = calculadoraCoeficientes(ecuacion,incognitasAnteriores,t);
+        float incognita = calculadoraCoeficientesJacobi(ecuacion,incognitasAnteriores,t);
         incognitasActuales.push_back(incognita);
         t++;
     }
@@ -74,11 +75,41 @@ for (const list<float>& ecuacion : ecuacionesConCoeficientes) {
     scanf("%d",&iteracion);
 }
 
+} else if(rtaMetodo == 2){
+
+list <float> incognitasAnteriores(rtaSist,0);
+list<float> incognitasActuales;
+
+while (iteracion != -1){
+int t = 0;
+
+for (const list<float>& ecuacion : ecuacionesConCoeficientes) {
+        float incognita = calculadoraCoeficientesGauss_Seidel(ecuacion,incognitasAnteriores,t,incognitasActuales);
+        incognitasActuales.push_back(incognita);
+        t++;
+    }
+    incognitasAnteriores = incognitasActuales;
+    
+
+    printf("Solucion: (");
+    for (auto it = incognitasActuales.begin(); it != incognitasActuales.end(); ++it) {
+        printf("%.2f", *it);
+        if (next(it) != incognitasActuales.end()) {
+            printf(";");
+        }
+    }
+    printf(")\n");
+    incognitasActuales.clear();
+    printf("Siguiente iteracion? Si = 1, No = -1.\n");
+    scanf("%d",&iteracion);
+}
+
+    
 }
 
 }
 
-float calculadoraCoeficientes(const list<float>& coeficientes, const list<float>& valores_anteriores, int posicionIncognita) {
+float calculadoraCoeficientesJacobi(const list<float>& coeficientes, const list<float>& valores_anteriores, int posicionIncognita) {
     auto itCoef = coeficientes.begin();
     auto itVal = valores_anteriores.begin();
 
@@ -105,228 +136,38 @@ float calculadoraCoeficientes(const list<float>& coeficientes, const list<float>
     return (terminoIndependiente - sumatoria) / coefIncognita;
 }
 
-/* 
+float calculadoraCoeficientesGauss_Seidel(const list<float>& coeficientes, const list<float>& valores_anteriores, int posicionIncognita, const list<float>& valores_actuales) {
+    auto itCoef = coeficientes.begin();
+    auto itVal_anteriores = valores_anteriores.begin();
+    auto itVal_actuales = valores_actuales.begin();
 
-ax + by = c
-dx + ey = f
+    float sumatoria_anteriores = 0.0, sumatoria_actuales = 0.0;
+    int j = 0;
 
-if(respuesta1 == 1){//Jacobi
+    int n = valores_anteriores.size();
 
-    while(iteracion != -1){
+    // valores_actuales = [1,2]
+    // valores_anteriores = [2,4,5,6]
 
-        num_iteracion ++;
+    // Recorrer coeficientes hasta la penúltima posición (coeficientes de variables)
+    for (; j < n; ++j, ++itCoef, ++itVal_anteriores) {
+        if (j != posicionIncognita) {
 
-        x_anterior = x;
-        y_anterior = y;
-
-        x = (c - b*y_anterior)/a;
-        y = (f - d*x_anterior)/e;
-
-        printf("Iteracion %d: (%f;%f)\n",num_iteracion,x,y);
-
-        printf("Responder 1 para seguir o -1 para parar\n");
-        scanf("%d",&iteracion);
-
+            if(valores_actuales.size() > j){
+            sumatoria_actuales += (*itCoef) * (*itVal_actuales);
+            } else {
+            sumatoria_anteriores += (*itCoef) * (*itVal_anteriores);
+            }
+        }
     }
-*/
 
-/*
-int iteracion = 0;
+    // Ahora itCoef apunta al coef independiente (último de la fila)
+    float terminoIndependiente = *itCoef;
 
-list<float> valor_incognitas_anteriores(coeficientes.size()-1, 0);
-list <float> valor_incognitas_actual(coeficientes.size()-1,0);
-if(rtaMetodo == 1){
+    // Obtener coeficiente de la incógnita actual (volver a empezar y avanzar hasta posicionIncognita)
+    itCoef = coeficientes.begin();
+    advance(itCoef, posicionIncognita);
+    float coefIncognita = *itCoef;
 
-while(iteracion != -1){
-
-// coeficientes --> [1,2,3,4,5,6]
-// valor_incognitas_anteriores --> [0,0,0,0,0]
-// valor_incognitas_actual --> [0,0,0,0,0]
-// valor_incognitas_actual[6 - (2*0 + 3*0 + 4*0 + 5*0),..,..,..,..]
-
-for(int j = 0; j < valor_incognitas_actual.size(); j++){ // [1,2,3,4,5,6] <-- coeficientes [0,0,0,0,0] <-- incognitas
-
-    auto it = valor_incognitas_actual.begin();
-    advance(it, j);
-    
-    *it = calculadoraCoeficientes(coeficientes,valor_incognitas_anteriores,j);
-
+    return (terminoIndependiente - sumatoria_anteriores - sumatoria_actuales) / coefIncognita;
 }
-int k = 0;
-for(int valorActual : valor_incognitas_actual){
-    if(k==0){
-    printf("Iteracion %d: (%f",valorActual);
-    }else if (k == valor_incognitas_actual.size() - 1){
-    printf("%f)\n",valorActual);
-    }else{
-        printf(";%f",valorActual);
-    }
-       k++; 
-    }
-    printf("Responder 1 para seguir o -1 para parar\n");
-        scanf("%d",&iteracion);
-}
-
-}
-
-/* 
-while(iteracion != -1){
-
-        num_iteracion ++;
-
-        x_anterior = x;
-        y_anterior = y;
-
-        x = (c - b*y_anterior)/a;
-        y = (f - d*x_anterior)/e;
-
-        printf("Iteracion %d: (%f;%f)\n",num_iteracion,x,y);
-
-        printf("Responder 1 para seguir o -1 para parar\n");
-        scanf("%d",&iteracion);
-
-    }
-
-
-
-
-
-switch(rtaSist){
-    case 1:{
-    float a,b,c,d,e,f;
-
-    cout << "Teniendo en cuenta el formato ax + by = c" << endl;
-
-    cout << "Coeficientes de la primer ecuacion (donde c es el termino independiente): " << endl;
-    cin >> a;
-    cin >> b;
-    cin >> c;
-
-    cout << "Teniendo en cuenta el formato dx + ey = f" << endl;
-
-    cout << "Coeficientes de la segunda ecuacion (donde f es el termino independiente): " << endl;
-    cin >> d;
-    cin >> e;
-    cin >> f;
-        
-    float x = 0;
-    float y = 0;
-
-    float x_anterior = 0;
-    float y_anterior = 0;
-
-    int num_iteracion = 0;
-
-    printf("Iteracion %d: (%f;%f)\n",num_iteracion,x,y);
-
-    if(respuesta1 == 1){//Jacobi
-
-    while(iteracion != -1){
-
-        num_iteracion ++;
-
-        x_anterior = x;
-        y_anterior = y;
-
-        x = (c - b*y_anterior)/a;
-        y = (f - d*x_anterior)/e;
-
-        printf("Iteracion %d: (%f;%f)\n",num_iteracion,x,y);
-
-        printf("Responder 1 para seguir o -1 para parar\n");
-        scanf("%d",&iteracion);
-
-    }
-    }
-    else{
-        while(iteracion != -1){//Gauss-Seidel
-
-            num_iteracion ++;
-    
-            x = (c - b*y)/a;
-            y = (f - d*x)/e;
-    
-            printf("Iteracion %d: (%f;%f)\n",num_iteracion,x,y);
-    
-            printf("Responder 1 para seguir o -1 para parar\n");
-            scanf("%d",&iteracion);
-    
-        }  
-    }
-    break;
-}
-    case 2:{
-
-        float a,b,c,d,e,f,g,h,i,j,k,l;
-
-    cout << "Teniendo en cuenta el formato ax + by + cz= d" << endl;
-
-    cout << "Coeficientes de la primer ecuacion (donde c es el termino independiente): " << endl;
-    cin >> a >> b >> c >> d;
-
-    cout << "Teniendo en cuenta el formato ex + fy + gz = h" << endl;
-
-    cout << "Coeficientes de la segunda ecuacion (donde f es el termino independiente): " << endl;
-    cin >> e >> f >> g >> h;
-
-    cout << "Teniendo en cuenta el formato ix + jy + kz = l" << endl;
-
-    cout << "Coeficientes de la segunda ecuacion (donde f es el termino independiente): " << endl;
-    cin >> i >> j >> k >> l;
-
-    float x = 0;
-    float y = 0;
-    float z = 0;
-
-    float x_anterior = 0;
-    float y_anterior = 0;
-    float z_anterior = 0;
-
-    int num_iteracion = 0;
-
-    printf("Iteracion %d: (%f;%f;%f)\n",num_iteracion,x,y,z);
-
-    if(respuesta1 == 1){ //Jacobi
-
-    while(iteracion != -1){
-
-        num_iteracion ++;
-
-        x_anterior = x;
-        y_anterior = y;
-        z_anterior = z;
-
-        x = (d - b*y_anterior - c*z_anterior)/a;
-        y = (h - e*x_anterior - g*z_anterior)/f;
-        z = (l - i*x_anterior - j*y_anterior)/k;
-
-        printf("Iteracion %d: (%f;%f;%f)\n",num_iteracion,x,y,z);
-
-        printf("Responder 1 para seguir o -1 para parar\n");
-        scanf("%d",&iteracion);
-
-    }
-
-    }else{//Gauss-Seidel
-        while(iteracion != -1){
-
-            num_iteracion ++;
-    
-            x_anterior = x;
-            y_anterior = y;
-            z_anterior = z;
-    
-            x = (d - b*y_anterior - c*z_anterior)/a;
-            y = (h - e*x - g*z_anterior)/f;
-            z = (l - i*x - j*y)/k;
-    
-            printf("Iteracion %d: (%f;%f;%f)\n",num_iteracion,x,y,z);
-    
-            printf("Responder 1 para seguir o -1 para parar\n");
-            scanf("%d",&iteracion);
-    }
-        break;
-    }
-}
-}
-*/
